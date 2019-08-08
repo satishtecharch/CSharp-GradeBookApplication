49f401fdbb69befcb28151ed0d5a209e2d7141d6
satishtecharch <53920009+satishtecharch@users.noreply.github.com>
2019-08-09 00:12:31 +0100

    Create GradeBookType.cs

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,13 @@
+﻿namespace GradeBook.Enums
+{
+    //Create a new Enum GradeBookType containing the types "Standard", "Ranked", "ESNU", "OneToFour", and "SixPoint".
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+
+    }
+}
