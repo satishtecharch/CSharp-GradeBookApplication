9710e3123c233ade85e279de11f085d89d56e3cd
satishtecharch <53920009+satishtecharch@users.noreply.github.com>
2019-08-13 21:55:14 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,7 +11,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade (double averageGrade)
         {
-            if (Students.Count <=5)
+            if (Students.Count <5)
             {
                 throw new InvalidOperationException("Less than 5 students");
             }
